1562f52b68c9dcebb021a1c69b3cca41626b4f12
Kaung Myat Han <arkarhtethan@gmail.com>
2023-09-16 20:51:21 +0630

    feat(): project finished

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["media.npr.org", "crests.football-data.org", "ichef.bbci.co.uk"],
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
+
+const Navbar: FC = () => {
+  return (
+    <div className="flex justify-between items-center py-2">
+      <Link href="/" className="flex items-center space-x-2">
+        <div className="relative w-[30px] h-[30px]">
+          <Image
+            src="/football-info.png"
+            alt="icon"
+            fill
+            className="object-cover"
+          />
+        </div>
+        <span className="text-2xl font-bold none md:block">Football info</span>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
